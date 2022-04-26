9852312841f2860e324cc69b450a84027f6adace
aluiziocatao <aluizio2210@gmail.com>
2022-04-25 21:59:09 -0500

    First Commit

---
+++ sgcmapi/src/main/java/br/ufac/sgcmapi/controller/ExemploController.java
@@ -0,0 +1,15 @@
+package br.ufac.sgcmapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ExemploController {
+    
+    @RequestMapping("/")
+    @ResponseBody
+    public String exemplo(){
+        return "Spring Boot!";
+    }
+}
